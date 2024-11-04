d3c33add899ac515261f5033c88df6ad370a0a1a
Shariq Khan <khanshariq7654@gmail.com>
2024-11-05 00:19:13 +0530

    commented out DM's and Home Icon from sidebar

--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -9,8 +9,8 @@ export const Sidebar = () => {
   return (
     <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-[9px] pb-[14px]">
       <WorkspaceSwitcher />
-      <SidebarButton icon={Home} label="Home" isActive />
-      <SidebarButton icon={MessagesSquare} label="DMs" />
+      {/* <SidebarButton icon={Home} label="Home" isActive /> */}
+      {/* <SidebarButton icon={MessagesSquare} label="DMs" /> */}
       <SidebarButton icon={Bell} label="Activity" />
       <SidebarButton icon={MoreHorizontal} label="More" />
       <div className="flex flex-col gap-y-1 items-center justify-center mt-auto">
